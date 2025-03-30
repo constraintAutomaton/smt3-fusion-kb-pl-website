ac3a04637c93e8ad886a32a90685004e6e1cd6c4
constraintAutomaton <Vous@exemple.com>
2025-03-30 21:46:00 +0200

    progression in the text box

--- src/component/CommandBloc.svelte
+++ src/component/CommandBloc.svelte
@@ -1,26 +1,57 @@
 <script lang="ts">
 	import { get_demon_knowledge_base, get_rule_knowledge_base } from '$lib';
 
-	let history:string[] = [];
+	let history: string[] = [];
+	let current_command = $state('');
 
-	let historyText = $state('');
+	let history_text = $state('');
 
-	async function demon_knowledge_base_in_history_box() {
-		historyText = await get_demon_knowledge_base();
+	let demon_info_button_clicked: boolean = false;
+	let rule_button_clicked: boolean = false;
+
+	async function demon_knowledge_base_in_history_box(): Promise<void> {
+		if (demon_info_button_clicked) {
+			history_text = history.join('\n');
+		} else {
+			history_text = await get_demon_knowledge_base();
+		}
+
+		demon_info_button_clicked = !demon_info_button_clicked;
 	}
 
-	async function rule_knowledge_base_in_history_box() {
-		historyText = await get_rule_knowledge_base();
+	async function rule_knowledge_base_in_history_box(): Promise<void> {
+		if (rule_button_clicked) {
+			history_text = history.join('\n');
+		} else {
+			history_text = await get_rule_knowledge_base();
+		}
+		rule_button_clicked = !rule_button_clicked;
+	}
+
+	function handleKeydown(event: KeyboardEvent): void {
+		if (event.key === 'Enter') {
+			event.preventDefault();
+			history.push(current_command);
+			current_command = '';
+		}
 	}
 </script>
 
 <div class="command-box">
-	<textarea class="history-box" readonly>{historyText}</textarea>
-	<input type="text" class="input-box" value="?- " />
+	<textarea class="history-box" readonly>{history_text}</textarea>
+	<div>
+		<span style="display: inline-block;">?- </span>
+		<textarea
+			class="input-box"
+			style="display: inline-block;"
+			onkeydown={handleKeydown}
+			bind:value={current_command}
+		></textarea>
+	</div>
 </div>
 
 <div>
-	<button onclick={demon_knowledge_base_in_history_box}>Show Demon Info</button>
+	<button class="show-button" onclick={demon_knowledge_base_in_history_box}>Show Demon Info</button>
 	<button onclick={rule_knowledge_base_in_history_box}>Show Rules</button>
 </div>
 
@@ -42,5 +73,7 @@
 	.input-box {
 		border: none;
 		outline: none;
+		resize: none;
+		height: 1em;
 	}
 </style>
