914540923daf8f415c0affe6494abd038a528cb6
constraintAutomaton <Vous@exemple.com>
2025-04-06 11:00:53 +0200

    minimally working UI

--- src/component/CommandBloc.svelte
+++ src/component/CommandBloc.svelte
@@ -1,19 +1,44 @@
 <script lang="ts">
 	import { get_demon_knowledge_base, get_rule_knowledge_base } from '$lib';
+	import { onMount } from 'svelte';
 
 	let history: string[] = [];
 	let current_command = $state('');
 
-	let history_text = $state('');
+	let inputBox: HTMLTextAreaElement;
+
+	let logText = $state('');
 
 	let demon_info_button_clicked: boolean = false;
 	let rule_button_clicked: boolean = false;
+	let wasCtrl:boolean =false;
+
+	function handleGlobalKey(event: KeyboardEvent):void {
+		const isCtrl = event.ctrlKey || event.metaKey;
+		if(wasCtrl && event.key == "v"){
+			inputBox.focus();
+		}
+		if (isCtrl) {
+			wasCtrl = true
+			return;
+		}
+		inputBox.focus();
+	}
+
+	function putHistoryIntoLogBox(): void {
+		logText = history.join('\n');
+	}
+
+	onMount(() => {
+		window.addEventListener('keydown', handleGlobalKey);
+		return () => window.removeEventListener('keydown', handleGlobalKey);
+	});
 
 	async function demon_knowledge_base_in_history_box(): Promise<void> {
 		if (demon_info_button_clicked) {
-			history_text = history.join('\n');
+			putHistoryIntoLogBox();
 		} else {
-			history_text = await get_demon_knowledge_base();
+			logText = await get_demon_knowledge_base();
 		}
 
 		demon_info_button_clicked = !demon_info_button_clicked;
@@ -21,30 +46,32 @@
 
 	async function rule_knowledge_base_in_history_box(): Promise<void> {
 		if (rule_button_clicked) {
-			history_text = history.join('\n');
+			putHistoryIntoLogBox();
 		} else {
-			history_text = await get_rule_knowledge_base();
+			logText = await get_rule_knowledge_base();
 		}
 		rule_button_clicked = !rule_button_clicked;
 	}
 
-	function handleKeydown(event: KeyboardEvent): void {
+	function handleKeydownInputBox(event: KeyboardEvent): void {
 		if (event.key === 'Enter') {
 			event.preventDefault();
 			history.push(current_command);
 			current_command = '';
+			putHistoryIntoLogBox();
 		}
 	}
 </script>
 
 <div class="command-box">
-	<textarea class="history-box" readonly>{history_text}</textarea>
+	<textarea class="log-box" readonly>{logText}</textarea>
 	<div>
 		<span style="display: inline-block;">?- </span>
 		<textarea
 			class="input-box"
 			style="display: inline-block;"
-			onkeydown={handleKeydown}
+			onkeydown={handleKeydownInputBox}
+			bind:this={inputBox}
 			bind:value={current_command}
 		></textarea>
 	</div>
@@ -62,7 +89,7 @@
 		border-style: solid;
 	}
 
-	.history-box {
+	.log-box {
 		border: none;
 		outline: none;
 		resize: none;
